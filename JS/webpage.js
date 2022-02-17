a663c4faa600e70be7a75e81acf1216c6518b936
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2022-02-17 10:20:06 -0500

    NodeModule Changes
    adding animeJS and Greensock

--- JS/webpage.js
+++ JS/webpage.js
@@ -3,4 +3,14 @@ function hideAndShow(show, hide){
     document.getElementById(show).style.display='block';
     document.getElementById(hide).style.display='none';
     return false;
-}
+};
+
+anime({
+    targets: ['TITLE.svg'],
+    points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
+    baseFrequency: 0,
+    scale: 1,
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutExpo'
+});
