fbb24d043e59612c28bdcdf89090075fdd1cb2a6
Lauren Casale <lcc4935@rit.edu>
2022-03-30 20:29:58 -0400

    almost there

--- JS/dragAndDrop.js
+++ JS/dragAndDrop.js
@@ -6,13 +6,33 @@ mobile:
 https://medium.com/@deepakkadarivel/drag-and-drop-dnd-for-mobile-browsers-fc9bcd1ad3c5
 */
 
-let squareFilled = false;
-let triangleFilled = false;
-let circleFilled = false;
-let hexagonFilled = false;
+let audio = document.getElementById('slotsAudio');
+
+let squareFilledBool = false;
+let triangleFilledBool = false;
+let circleFilledBool = false;
+let hexagonFilledBool = false;
 
 completeButton();
 tryAgainButton();
+
+function ranomize(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
 
 
 let deviceWidth = document.getElementById("container").offsetWidth;
@@ -24,20 +44,20 @@ let triangleSlot = document.getElementById('triangleSlot');
 let circleSlot = document.getElementById('circleSlot');
 let hexagonSlot = document.getElementById('hexagonSlot');
 
-let squareSlotWidth = squareSlot.offsetWidth;
-let triangleSlotWidth = triangleSlot.offsetWidth;
-let circleSlotWidth = circleSlot.offsetWidth;
-let hexagonSlotWidth = hexagonSlot.offsetWidth;
+let slotWidth = squareSlot.offsetWidth; //all slot have same width and height
 
-squareSlot.style.left = (deviceWidth / 2 - squareSlotWidth / 2) + "px";
-triangleSlot.style.left = (deviceWidth / 2 - triangleSlotWidth / 2) + "px";
-circleSlot.style.left = (deviceWidth / 2 - circleSlotWidth / 2) + "px";
-hexagonSlot.style.left = (deviceWidth / 2 - hexagonSlotWidth / 2) + "px";
+let slotsArray=[squareSlot, triangleSlot, circleSlot, hexagonSlot];
+ranomize(slotsArray);
 
-squareSlot.style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 4) + "px";
-triangleSlot.style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 3) + "px";
-circleSlot.style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 2) + "px";
-hexagonSlot.style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 1) + "px";
+slotsArray[0].style.left = (deviceWidth / 2 - slotWidth / 2) + "px";
+slotsArray[1].style.left = (deviceWidth / 2 - slotWidth / 2) + "px";
+slotsArray[2].style.left = (deviceWidth / 2 - slotWidth / 2) + "px";
+slotsArray[3].style.left = (deviceWidth / 2 - slotWidth / 2) + "px";
+
+slotsArray[0].style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 4) + "px";
+slotsArray[1].style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 3) + "px";
+slotsArray[2].style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 2) + "px";
+slotsArray[3].style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 1) + "px";
 
 //gem positions
 let squareGem = document.getElementById('squareGem');
@@ -45,20 +65,20 @@ let triangleGem = document.getElementById('triangleGem');
 let circleGem = document.getElementById('circleGem');
 let hexagonGem = document.getElementById('hexagonGem');
 
-let squareGemWidth = squareGem.offsetWidth;
-let triangleGemWidth = triangleGem.offsetWidth;
-let circleGemWidth = circleGem.offsetWidth;
-let hexagonGemWidth = hexagonGem.clientWidth;
+let gemWidth = squareGem.offsetWidth; //all gems have same width and height
 
-squareGem.style.left = (deviceWidth / 2 - squareGemWidth / 2) + "px";
-triangleGem.style.left = (deviceWidth / 2 - triangleGemWidth / 2) + "px";
-circleGem.style.left = (deviceWidth / 2 - circleGemWidth / 2) + "px";
-hexagonGem.style.left = (deviceWidth / 2 - hexagonGemWidth / 2) + "px";
+let gemsArray = [squareGem, triangleGem, circleGem, hexagonGem]
+ranomize(gemsArray);
 
-squareGem.style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 4) + "px";
-triangleGem.style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 3) + "px";
-circleGem.style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 2) + "px";
-hexagonGem.style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 1) + "px";
+gemsArray[0].style.left = (deviceWidth / 2 - gemWidth * 2.3) + "px";
+gemsArray[1].style.left = (deviceWidth / 2 + gemWidth) + "px";
+gemsArray[2].style.left = (deviceWidth / 2 - gemWidth * 1.8) + "px";
+gemsArray[3].style.left = (deviceWidth / 2 + gemWidth * 1.2) + "px";
+
+gemsArray[0].style.top = (deviceHeight - deviceHeight / 12 - (deviceHeight / 6) * 4) + "px";
+gemsArray[1].style.top = (deviceHeight - deviceHeight / 10 - (deviceHeight / 6) * 3) + "px";
+gemsArray[2].style.top = (deviceHeight - deviceHeight / 8 - (deviceHeight / 6) * 2) + "px";
+gemsArray[3].style.top = (deviceHeight - deviceHeight / 6 - (deviceHeight / 6) * 1) + "px";
 
 //onload
 window.onload = function () {
@@ -92,6 +112,7 @@ window.onload = function () {
 
 }
 
+
 //Square
 squareGem.addEventListener('touchend', function (e) {
     let squareSlotX = parseInt(squareSlot.style.left);
@@ -100,14 +121,27 @@ squareGem.addEventListener('touchend', function (e) {
     let squareGemX = parseInt(squareGem.style.left);
     let squareGemY = parseInt(squareGem.style.top);
 
-    let squareSlotXBound = squareSlotX + squareSlotWidth;
-    let squareSlotYBound = squareSlotY + squareSlotWidth;
+    let squareSlotXBound = squareSlotX + slotWidth;
+    let squareSlotYBound = squareSlotY + slotWidth;
 
-    let squareGemXBound = squareGemX + squareGemWidth / 2;
-    let squareGemYBound = squareGemY + squareGemWidth / 2;
+    let squareGemXBound = squareGemX + gemWidth / 2;
+    let squareGemYBound = squareGemY + gemWidth / 2;
 
     if (squareGemXBound >= squareSlotX && squareGemXBound <= squareSlotXBound && squareGemYBound >= squareSlotY && squareGemYBound <= squareSlotYBound) {
-        console.log("square true");
+
+        squareSlot.src = "../media/ddImages/filled/squareFilled.png";
+        squareGem.style.visibility = 'hidden';
+        squareSlot.style.width = '16vh';
+        squareSlot.style.marginLeft = '-3vh';
+        squareSlot.style.marginTop = '-3vh';
+
+        audio.play();
+
+        squareFilledBool = true;
+
+        completeButton();
+        tryAgainButton();
+
     } else {
         console.log("square false");
     }
@@ -122,14 +156,27 @@ triangleGem.addEventListener('touchend', function (e) {
     let triangleGemX = parseInt(triangleGem.style.left);
     let triangleGemY = parseInt(triangleGem.style.top);
 
-    let triangleSlotXBound = triangleSlotX + triangleSlotWidth;
-    let triangleSlotYBound = triangleSlotY + triangleSlotWidth;
+    let triangleSlotXBound = triangleSlotX + slotWidth;
+    let triangleSlotYBound = triangleSlotY + slotWidth;
 
-    let triangleGemXBound = triangleGemX + triangleGemWidth / 2;
-    let triangleGemYBound = triangleGemY + triangleGemWidth / 2;
+    let triangleGemXBound = triangleGemX + gemWidth / 2;
+    let triangleGemYBound = triangleGemY + gemWidth / 2;
 
     if (triangleGemXBound >= triangleSlotX && triangleGemXBound <= triangleSlotXBound && triangleGemYBound >= triangleSlotY && triangleGemYBound <= triangleSlotYBound) {
-        console.log("triangle true");
+
+        triangleSlot.src = "../media/ddImages/filled/triangleFilled.png";
+        triangleGem.style.visibility = 'hidden';
+        triangleSlot.style.width = '16vh';
+        triangleSlot.style.marginLeft = '-3vh';
+        triangleSlot.style.marginTop = '-3vh';
+
+        audio.play();
+
+        triangleFilledBool = true;
+
+        completeButton();
+        tryAgainButton();
+
     } else {
         console.log("triangle false");
     }
@@ -144,14 +191,27 @@ circleGem.addEventListener('touchend', function (e) {
     let circleGemX = parseInt(circleGem.style.left);
     let circleGemY = parseInt(circleGem.style.top);
 
-    let circleSlotXBound = circleSlotX + circleSlotWidth;
-    let circleSlotYBound = circleSlotY + circleSlotWidth;
+    let circleSlotXBound = circleSlotX + slotWidth;
+    let circleSlotYBound = circleSlotY + slotWidth;
 
-    let circleGemXBound = circleGemX + circleGemWidth / 2;
-    let circleGemYBound = circleGemY + circleGemWidth / 2;
+    let circleGemXBound = circleGemX + gemWidth / 2;
+    let circleGemYBound = circleGemY + gemWidth / 2;
 
     if (circleGemXBound >= circleSlotX && circleGemXBound <= circleSlotXBound && circleGemYBound >= circleSlotY && circleGemYBound <= circleSlotYBound) {
-        console.log("circle true");
+
+        circleSlot.src = "../media/ddImages/filled/circleFilled.png";
+        circleGem.style.visibility = 'hidden';
+        circleSlot.style.width = '16vh';
+        circleSlot.style.marginLeft = '-3vh';
+        circleSlot.style.marginTop = '-3vh';
+
+        audio.play();
+
+        circleFilledBool = true;
+
+        completeButton();
+        tryAgainButton();
+
     } else {
         console.log("circle false");
     }
@@ -166,148 +226,39 @@ hexagonGem.addEventListener('touchend', function (e) {
     let hexagonGemX = parseInt(hexagonGem.style.left);
     let hexagonGemY = parseInt(hexagonGem.style.top);
 
-    let hexagonSlotXBound = hexagonSlotX + hexagonSlotWidth;
-    let hexagonSlotYBound = hexagonSlotY + hexagonSlotWidth;
+    let hexagonSlotXBound = hexagonSlotX + slotWidth;
+    let hexagonSlotYBound = hexagonSlotY + slotWidth;
 
-    let hexagonGemXBound = hexagonGemX + hexagonGemWidth / 2;
-    let hexagonGemYBound = hexagonGemY + hexagonGemWdth / 2;
+    let hexagonGemXBound = hexagonGemX + gemWidth / 2;
+    let hexagonGemYBound = hexagonGemY + gemWidth / 2;
 
     if (hexagonGemXBound >= hexagonSlotX && hexagonGemXBound <= hexagonSlotXBound && hexagonGemYBound >= hexagonSlotY && hexagonGemYBound <= hexagonSlotYBound) {
-        console.log("hexagon true");
+
+        hexagonSlot.src = "../media/ddImages/filled/hexagonFilled.png";
+        hexagonGem.style.visibility = 'hidden';
+        hexagonSlot.style.width = '16vh';
+        hexagonSlot.style.marginLeft = '-3vh';
+        hexagonSlot.style.marginTop = '-3vh';
+
+        audio.play();
+
+        hexagonFilledBool = true;
+
+        completeButton();
+        tryAgainButton();
+
     } else {
         console.log("hexagon false");
     }
 
 })
 
-// /* draggable element */
-// const itemSquare = document.querySelector('.itemSquare');
-// itemSquare.addEventListener('dragstart', dragStart);
-
-// const itemTriangle = document.querySelector('.itemTriangle');
-// itemTriangle.addEventListener('dragstart', dragStart);
-
-// const itemCircle = document.querySelector('.itemCircle');
-// itemCircle.addEventListener('dragstart', dragStart);
-
-// const itemHexagon = document.querySelector('.itemHexagon');
-// itemHexagon.addEventListener('dragstart', dragStart);
-
-// function dragStart(e) {
-//     e.dataTransfer.setData('text/plain', e.target.id);
-//     setTimeout(() => {
-//         e.target.classList.add('hide');
-//     }, 0);
-// }
-
-
-// /* drop targets */
-// const squares = document.querySelectorAll('.square');
-// squares.forEach(square => {
-//     square.addEventListener('dragenter', dragEnter)
-//     square.addEventListener('dragover', dragOver);
-//     square.addEventListener('dragleave', dragLeave);
-//     square.addEventListener('drop', drop);
-// });
-
-// const triangles = document.querySelectorAll('.triangle');
-// triangles.forEach(triangle => {
-//     triangle.addEventListener('dragenter', dragEnter)
-//     triangle.addEventListener('dragover', dragOver);
-//     triangle.addEventListener('dragleave', dragLeave);
-//     triangle.addEventListener('drop', drop);
-// });
-
-// const circles = document.querySelectorAll('.circle');
-// circles.forEach(circle => {
-//     circle.addEventListener('dragenter', dragEnter)
-//     circle.addEventListener('dragover', dragOver);
-//     circle.addEventListener('dragleave', dragLeave);
-//     circle.addEventListener('drop', drop);
-// });
-
-// const hexagons = document.querySelectorAll('.hexagon');
-// hexagons.forEach(hexagon => {
-//     hexagon.addEventListener('dragenter', dragEnter)
-//     hexagon.addEventListener('dragover', dragOver);
-//     hexagon.addEventListener('dragleave', dragLeave);
-//     hexagon.addEventListener('drop', drop);
-// });
-
-// function dragEnter(e) {
-//     e.preventDefault();
-//     e.target.classList.add('drag-over');
-// }
-
-// function dragOver(e) {
-//     e.preventDefault();
-//     e.target.classList.add('drag-over');
-// }
-
-// function dragLeave(e) {
-//     e.target.classList.remove('drag-over');
-// }
-
-function drop(e) {
-    e.target.classList.remove('drag-over');
-
-    // get the draggable element
-    const id = e.dataTransfer.getData('text/plain');
-    const draggable = document.getElementById(id);
-
-    console.log("id" + id);
-    console.log("class" + e.path[1].className);
-
-    if (id == 'squareGem' && (e.path[1].className == 'drop-targetsSquare' || e.path[1].className == 'square')) {
-        // let element = document.getElementsByClassName('triangle');
-        // element.style.backgroundColor = "black";
-        squareFilled = true;
-        console.log('square');
-        console.log(squareFilled);
-    }
-
-    if (id == 'triangleGem' && (e.path[1].className == 'drop-targetsTriangle' || e.path[1].className == 'triangle')) {
-        // let element = document.getElementsByClassName('triangle');
-        // element.style.backgroundColor = "black";
-        triangleFilled = true;
-        console.log('tri');
-        console.log(triangleFilled);
-    }
-
-    if (id == 'circleGem' && (e.path[1].className == 'drop-targetsCircle' || e.path[1].className == 'circle')) {
-        // let element = document.getElementsByClassName('triangle');
-        // element.style.backgroundColor = "black";
-        circleFilled = true;
-        console.log('circle');
-        console.log(circleFilled);
-    }
-
-    if (id == 'hexagonGem' && (e.path[1].className == 'drop-targetsHexagon' || e.path[1].className == 'hexagon')) {
-        // let element = document.getElementsByClassName('triangle');
-        // element.style.backgroundColor = "black";
-        hexagonFilled = true;
-        console.log('hex');
-        console.log(hexagonFilled);
-    }
-
-    completeButton();
-    tryAgainButton();
-
-    // add it to the drop target
-    e.target.appendChild(draggable);
-
-    // display the draggable element
-    draggable.classList.remove('hide');
-
-    //replace slot-image with stone-in-slot image
-}
-
 /** Buttons */
 
 function completeButton() {
-    if (squareFilled == true && triangleFilled == true && circleFilled == true && hexagonFilled == true) {
+    if (squareFilledBool == true && triangleFilledBool == true && circleFilledBool == true && hexagonFilledBool == true) {
         document.getElementById('complete').style.visibility = 'visible';
-        //document.getElementById('container').style.background-image = url("../media/ddImages/BackCrystal-Default.png");
+        document.getElementById('container').style.backgroundImage = "url(../media/ddImages/screen/crystalBright.png)";
     } else {
         document.getElementById('complete').style.visibility = 'hidden';
     }
