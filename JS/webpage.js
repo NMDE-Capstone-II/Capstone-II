806aed2b2a6c34f083f3b2ce3bbe103f8eb38228
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2022-04-12 08:36:01 -0400

    Bugfix  + QRCodes-Rewards
    minor fixes

--- JS/webpage.js
+++ JS/webpage.js
@@ -72,9 +72,13 @@ function instructional(evt, stepNum) {
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
+  
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(stepNum).style.display = "block";
   evt.currentTarget.className += " active";
+
+  document.getElementById("defaultOpen").click();
+
 }
