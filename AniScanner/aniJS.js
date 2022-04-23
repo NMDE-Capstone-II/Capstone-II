80d47776636e2019ecc42316d774d04c313f4a0a
Adit-COCO-Garg <adit.garg21k@gmail.com>
2022-04-23 09:50:45 -0400

    added videos and ani scanner works

--- AniScanner/aniJS.js
+++ AniScanner/aniJS.js
@@ -2,10 +2,10 @@ let resultContainer = document.getElementById('qr-reader-results');
 let h1tab = document.getElementById('qr-reader');
 let lastResult, countResults = 0;
 let videos = {
-    fire: "oT_XIgJoKE0",
-    earth: "oT_XIgJoKE0",
-    air: "oT_XIgJoKE0",
-    water: "oT_XIgJoKE0"
+    Fire: "Fire-Ani-SOUND.mp4",
+    earth: "Earth-Ani-SOUND.mp4",
+    air: "Wind-Ani-SOUND.mp4",
+    water: "Ice-Ani-SOUND.mp4"
 }
 
 
@@ -16,13 +16,12 @@ function onScanSuccess(decodedText, decodedResult) {
         // Handle on success condition with the decoded message.
         console.log(`Scan result ${decodedText}`, decodedResult, h1tab);
         // h1tab.style.visibility = "visible"
-        h1tab.innerHTML = `<iframe class="fullScreen"  src="https://www.youtube.com/embed/${videos[decodedText]}?autoplay=1&mute=1&controls=0&rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        `;
+        h1tab.innerHTML = ` <video autoplay="true" loop="true" src="./../Media/swordVids/${videos[decodedText]}"></video>`;
     }
 }
 
 let html5QrcodeScanner = new Html5QrcodeScanner(
-    "qr-reader", { fps: 10, qrbox: 250 });
+    "qr-reader", { fps: 10, qrbox: {width: 300, height: 300} });
 html5QrcodeScanner.render(onScanSuccess);
 
 
